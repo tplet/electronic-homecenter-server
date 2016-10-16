5f9e49aeb824e229f7e659a7e8d911b11d46cef5
Thibault PLET <thibault.plet@gmail.com>
2016-10-16 22:01:20 +0200

    Send OK response when receive a packet

--- src/service/ReceiverActionManager.h
+++ src/service/ReceiverActionManager.h
@@ -6,6 +6,7 @@
 #define HOMECENTER_SERVER_SERVICE_RECEIVERACTIONMANAGER_H
 
 #include <com/osteres/automation/action/ActionManagerBase.h>
+#include <com/osteres/automation/action/implement/packet/ResponseOkAction.h>
 #include <com/osteres/automation/action/implement/ConsoleDisplayPacket.h>
 #include <com/osteres/automation/transmission/Transmitter.h>
 #include <com/osteres/automation/transmission/packet/Command.h>
@@ -21,6 +22,7 @@
 
 using std::string;
 using com::osteres::automation::action::ActionManagerBase;
+using com::osteres::automation::action::implement::packet::ResponseOkAction;
 using com::osteres::automation::action::implement::ConsoleDisplayPacket;
 using com::osteres::automation::transmission::Transmitter;
 using com::osteres::automation::transmission::packet::Command;
@@ -73,6 +75,10 @@ namespace service
         {
             // Output packet to console (debug version) TODO: To remove in production
             this->actionDisplay->execute(packet);
+
+            // Send OK response
+            ResponseOkAction actionOk(this->transmitter);
+            actionOk.execute(packet, 0); // Use 0 for type, master has no type
 
             // Is packet has identifier ?
             // No: Ignore this packet and send a generated identifier
