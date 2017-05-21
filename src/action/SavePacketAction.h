aca296368b0bfcaa6581834ed6469f8660008f29
Thibault PLET <thibault.plet@gmail.com>
2017-05-21 19:04:02 +0200

    When save received packet, set timestamp if not defined

--- src/action/SavePacketAction.h
+++ src/action/SavePacketAction.h
@@ -10,11 +10,13 @@
 #include <service/DatabaseManager.h>
 #include <string>
 #include <iostream>
+#include <ctime>
 
 using std::string;
 using com::osteres::automation::action::Action;
 using com::osteres::automation::transmission::packet::Packet;
 using service::DatabaseManager;
+using std::time;
 
 namespace action
 {
@@ -36,10 +38,16 @@ namespace action
          */
         bool execute(Packet * packet)
         {
+            // If empty date, fill with local date
+            unsigned long int date = packet->getDate();
+            if (date == 0) {
+                date = this->getTimestamp();
+            }
+
             string query = "INSERT INTO `packet` ";
             query += "(`date`, `source_type`, `source_identifier`, `command`, `target`, `data_long1`, `data_long2`, `data_long3`, `data_long4`, `data_uchar1`, `data_uchar2`, `data_uchar3`, `data_char1`, `data_char2`, `data_char3`) ";
             query += "VALUES('" +
-                     to_string(packet->getDate()) + "', '" +
+                     to_string(date) + "', '" +
                      to_string(packet->getSourceType()) + "', '" + to_string(packet->getSourceIdentifier()) + "', '" +
                      to_string(packet->getCommand()) + "', '" +
                      to_string(packet->getTarget()) + "', '" +
@@ -56,6 +64,16 @@ namespace action
 
             this->executed = true;
             return true;
+        }
+
+        /**
+         * Get current timestamp
+         */
+        unsigned long int getTimestamp()
+        {
+            time_t t = time(0);
+            unsigned long int now = static_cast<unsigned long int> (t);
+            return now;
         }
 
     protected:
